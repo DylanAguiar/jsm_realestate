0b26180ada3915bbd1f297778c8209c8b67b6042
DylanAguiar <50515682+DylanAguiar@users.noreply.github.com>
2022-01-19 09:00:41 +0100

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,6 +8,15 @@ const Layout = ({ children }) => (
     <>
     <Head>
         <title>Real Estate</title>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZT5JFVKMHT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZT5JFVKMHT');
+</script>
     </Head>
     <Box maxWidth="1280px" m="auto">
         <header>
